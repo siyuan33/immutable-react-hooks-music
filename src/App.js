5b40662bb0156b51fc54b8340dc94bc04f2b770e
DESKTOP-VLDF3F3\Administrator <370455671@qq.com>
2021-01-28 02:57:02 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { GlobalStyle } from "./style"
+import { IconStyle } from "./assets/iconfont/iconfont"
+import { renderRoutes } from "react-router-config"
+import routes from "./routes/index.js"
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+      {renderRoutes(routes)}
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
